29da74bd9db8a56e6603ca38a65db258c640d792
Brian Ngetich <kipronobrian97@gmail.com>
2024-07-08 23:37:17 +0300

    Anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word) -> None:
+        self._word = word
+        self.anagrams = []
+
+    @property
+    def word(self):
+        return self._word
+
+    def match(self, words):
+        original_sorted = sorted(self._word)
+        for word in words:
+            if sorted(word) == original_sorted:
+                self.anagrams.append(word)
+        return self.anagrams
+
